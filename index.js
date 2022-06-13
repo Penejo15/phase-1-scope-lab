0c240258706a0137ddac0b5ac2e1ff3bcd730521
peter enejo <petstorm101@gmail.com>
2022-06-13 20:20:59 +0100

    commit tree 71ae23a9eb2465cb511879be2898cbb9c097945b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+
+   customerName = customerName.toUpperCase();
+
+   return customerName
+
+}
+
+
+
+var bestCustomer 
+
+function setBestCustomer(){
+
+      bestCustomer = 'not bob';
+
+      return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+      
+      bestCustomer = 'maybe bob';
+
+      return bestCustomer;
+      
+}
+
+const leastFavoriteCustomer = "I am moringa student"
+
+function changeLeastFavoriteCustomer(){
+   
+      leastFavoriteCustomer = "I didnt know moringa was this challenging"
+
+      return leastFavoriteCustomer
+}
